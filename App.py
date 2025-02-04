a2b087b07dd3d4bcaa44121eae480631f218f9a8
ppabam <tom@diginori.com>
2025-02-04 11:29:10 +0900

    group by

--- App.py
+++ App.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas as pd
 
 st.write("""
 # My first app
@@ -6,4 +7,7 @@ Hello **world!**
 
 ![img](https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSMDmITYUL3-dQiE99pPRG-f1gLO76Wh_UjEXTSvx0RiNdbvu_d4-L5OR43qlce8eNfeHld83WgawlwjNkQvWM7cA)
 """)
+
+df = pd.read_csv('note/menu.csv')
+df
 
