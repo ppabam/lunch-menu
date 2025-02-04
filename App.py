600a4090ae0837804d8932240a39e16c1bfd53ab
ppabam <tom@diginori.com>
2025-02-04 10:07:52 +0900

    streamlit

---
+++ App.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.write("""
+# My first app
+Hello **world!**
+
+![img](https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSMDmITYUL3-dQiE99pPRG-f1gLO76Wh_UjEXTSvx0RiNdbvu_d4-L5OR43qlce8eNfeHld83WgawlwjNkQvWM7cA)
+""")
+
