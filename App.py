8823e1d212000190c430f1e0ba4a07a864006234
ppabam <tom@diginori.com>
2025-02-04 15:45:48 +0900

    디비저장 ing ...

--- App.py
+++ App.py
@@ -1,12 +1,40 @@
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
+import psycopg
+
+DB_CONFIG = {
+    "user": "sunsin",
+    "dbname": "sunsindb",
+    "password": "mysecretpassword",
+    "host": "localhost",
+    "port": "5432"
+}
+
+def get_connection():
+    return psycopg.connect(**DB_CONFIG)
+
 
 st.title("순신점심기록장")
 st.subheader("입력")
 menu_name = st.text_input("메뉴 이름", placeholder="예: 김치찌게")
 member_name = st.text_input("먹은 사람", value="TOM")
 dt = st.date_input("얌얌 날짜")
+
+isPress = st.button("메뉴 저장")
+
+if isPress:
+    if menu_name and member_name and dt:
+        conn = get_connection()
+        cursor = conn.cursor()
+        cursor.execute(
+           "INSERT INTO lunch_menu (menu_name, member_name, dt) VALUES (%s, %s, %s);",
+            (menu_name, member_name, dt)
+        )
+
+        st.success(f"버튼{isPress}:{menu_name},{member_name},{dt}")
+    else:
+        st.warning(f"모든 값을 입력해주세요!")
 
 
 
