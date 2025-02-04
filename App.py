4b6e6b756cbda8f84329cb8f6043fd7b32e9520a
ppabam <tom@diginori.com>
2025-02-04 11:35:00 +0900

    to WEB

--- App.py
+++ App.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+import matplotlib.pyplot as plt
 
 st.write("""
 # My first app
@@ -9,5 +10,16 @@ Hello **world!**
 """)
 
 df = pd.read_csv('note/menu.csv')
-df
 
+start_idx = df.columns.get_loc('2025-01-07')
+melted_df = df.melt(id_vars=['ename'], value_vars=df.columns[start_idx:-2], 
+                     var_name='dt', value_name='menu')
+
+not_na_df = melted_df[~melted_df['menu'].isin(['-','x','<결석>'])]
+gdf = not_na_df.groupby('ename')['menu'].count().reset_index()
+#gdf.plot(x="ename", y="menu", kind="bar")
+
+# 📊 Matplotlib로 바 차트 그리기
+fig, ax = plt.subplots()
+gdf.plot(x="ename", y="menu", kind="bar", ax=ax)
+st.pyplot(fig)
