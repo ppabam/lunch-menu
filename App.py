6e4487241338d34feba632536a5b07ddb69d80e5
ppabam <tom@diginori.com>
2025-02-04 17:10:54 +0900

    숙제(전)

--- App.py
+++ App.py
@@ -16,6 +16,7 @@ def get_connection():
 
 
 st.title("순신점심기록장")
+
 st.subheader("입력")
 menu_name = st.text_input("메뉴 이름", placeholder="예: 김치찌게")
 member_name = st.text_input("먹은 사람", value="TOM")
@@ -39,15 +40,24 @@ if isPress:
         st.warning(f"모든 값을 입력해주세요!")
 
 
+st.subheader("확인")
+query = """SELECT 
+menu_name AS menu, 
+member_name AS ename, 
+dt 
+FROM lunch_menu 
+ORDER BY dt DESC"""
 
+conn = get_connection()
+cursor = conn.cursor()
+cursor.execute(query)
+rows = cursor.fetchall()
+#conn.commit()
+cursor.close()
 
-
-
-
-
-
-
-
+#selected_df = pd.DataFrame([[1,2,3],[4,5,6]], columns=['a','b','c'])
+select_df = pd.DataFrame(rows, columns=['menu','ename','dt'])
+select_df
 
 st.subheader("통계")
 df = pd.read_csv('note/menu.csv')
@@ -57,7 +67,9 @@ melted_df = df.melt(id_vars=['ename'], value_vars=df.columns[start_idx:-2],
                      var_name='dt', value_name='menu')
 
 not_na_df = melted_df[~melted_df['menu'].isin(['-','x','<결석>'])]
-gdf = not_na_df.groupby('ename')['menu'].count().reset_index()
+
+#gdf = not_na_df.groupby('ename')['menu'].count().reset_index()
+gdf = select_df.groupby('ename')['menu'].count().reset_index()
 #gdf.plot(x="ename", y="menu", kind="bar")
 
 gdf
@@ -67,3 +79,8 @@ gdf
 fig, ax = plt.subplots()
 gdf.plot(x="ename", y="menu", kind="bar", ax=ax)
 st.pyplot(fig)
+
+# TODO
+# CSV 로드해서 한번에 다 디비에 INSERT 하는거
+st.subheader("벌크 인서트")
+st.button("한방에 인서트")
