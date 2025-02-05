45ef73e967b4a1c3afff10e5f7c362bfa32e99d0
ppabam <tom@diginori.com>
2025-02-05 16:40:26 +0900

    중복 제외 조건 추가

--- App.py
+++ App.py
@@ -20,15 +20,19 @@ def get_connection():
     return psycopg.connect(**DB_CONFIG)
 
 def insert_menu(menu_name, member_name, dt):
-    conn = get_connection()
-    cursor = conn.cursor()
-    cursor.execute(
-        "INSERT INTO lunch_menu (menu_name, member_name, dt) VALUES (%s, %s, %s);",
+    try:
+        conn = get_connection()
+        cursor = conn.cursor()
+        cursor.execute(
+            "INSERT INTO lunch_menu (menu_name, member_name, dt) VALUES (%s, %s, %s);",
             (menu_name, member_name, dt)
-        )
-    conn.commit()
-    cursor.close()
-    conn.close()
+            )
+        conn.commit()
+        cursor.close()
+        conn.close()
+    except Exception as e:
+        print(f"Exception:{e}")
+
 
 st.title(f"순신점심기록장!{db_name}")
 
