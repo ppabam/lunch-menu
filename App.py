5024ee63aa4a386b82bb95ef72ad140eb87d5ded
ppabam <tom@diginori.com>
2025-02-05 11:42:49 +0900

    fun insert_menu

--- App.py
+++ App.py
@@ -18,6 +18,16 @@ DB_CONFIG = {
 def get_connection():
     return psycopg.connect(**DB_CONFIG)
 
+def insert_menu(menu_name, member_name, dt):
+    conn = get_connection()
+    cursor = conn.cursor()
+    cursor.execute(
+        "INSERT INTO lunch_menu (menu_name, member_name, dt) VALUES (%s, %s, %s);",
+            (menu_name, member_name, dt)
+        )
+    conn.commit()
+    cursor.close()
+    conn.close()
 
 st.title("순신점심기록장")
 
@@ -30,16 +40,8 @@ isPress = st.button("메뉴 저장")
 
 if isPress:
     if menu_name and member_name and dt:
-        conn = get_connection()
-        cursor = conn.cursor()
-        cursor.execute(
-           "INSERT INTO lunch_menu (menu_name, member_name, dt) VALUES (%s, %s, %s);",
-            (menu_name, member_name, dt)
-        )
-        conn.commit()
-        cursor.close()
-
-        st.success(f"버튼{isPress}:{menu_name},{member_name},{dt}")
+        insert_menu(menu_name, member_name, dt)
+        st.success(f"입력성공")
     else:
         st.warning(f"모든 값을 입력해주세요!")
 
