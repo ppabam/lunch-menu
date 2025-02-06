354704f15f122b88ce0ea1fb732b81cb8cbca458
ppabam <tom@diginori.com>
2025-02-06 12:41:34 +0900

    제약조건

--- App.py
+++ App.py
@@ -5,6 +5,8 @@ import psycopg
 import os
 from dotenv import load_dotenv
 
+members = {"SEO": 5, "TOM": 1, "cho": 2, "hyun": 3, "nuni": 10, "JERRY": 4, "jacob": 7, "jiwon": 6, "lucas": 9, "heejin": 8}
+
 # https://docs.streamlit.io/develop/concepts/connections/secrets-management
 load_dotenv()
 db_name = os.getenv("DB_NAME")
@@ -19,13 +21,14 @@ DB_CONFIG = {
 def get_connection():
     return psycopg.connect(**DB_CONFIG)
 
-def insert_menu(menu_name, member_name, dt):
+def insert_menu(menu_name, member_id, dt):
     try:
         conn = get_connection()
         cursor = conn.cursor()
         cursor.execute(
-            "INSERT INTO lunch_menu (menu_name, member_name, dt) VALUES (%s, %s, %s);",
-            (menu_name, member_name, dt)
+            """INSERT INTO lunch_menu (menu_name, member_name, dt) 
+            VALUES (%s, %s, %s);""",
+            (menu_name, member_id, dt)
             )
         conn.commit()
         cursor.close()
@@ -42,15 +45,19 @@ st.subheader("입력")
 menu_name = st.text_input("메뉴 이름", placeholder="예: 김치찌게")
 member_name = st.selectbox(
     "먹은 사람",
-    ("TOM", "KIM", "LEE"),
+    options=list(members.keys()),
+    index=list(members.keys()).index('TOM')
 )
+member_id = members[member_name]
+
 dt = st.date_input("얌얌 날짜")
 
 isPress = st.button("메뉴 저장")
 
 if isPress:
-    if menu_name and member_name and dt:
-        if insert_menu(menu_name, member_name, dt):
+    # member_name 을 member_id 로 바꾸어서 DB 에 id 가 insert 되록 해주세요
+    if menu_name and member_id and dt:
+        if insert_menu(menu_name, member_id, dt):
             st.success(f"입력성공")
         else:
             st.error(f"입력실패")
--- README.md
+++ README.md
@@ -46,6 +46,61 @@ values
 ('heejin'),
 ('lucas'),
 ('nuni');
+
+SELECT jsonb_object_agg(name, id) 
+FROM (
+	select name, id from member order by id
+) temp;
+
+{"SEO": 5, "TOM": 1, "cho": 2, "hyun": 3, "nuni": 10, "JERRY": 4, "jacob": 7, "jiwon": 6, "lucas": 9, "heejin": 8}
+
+-- 새로운 컬럼 추가
+alter table lunch_menu 
+add column member_id int;
+
+select * from lunch_menu limit 1;
+
+-- 기존의 제약조건을 삭제
+alter table lunch_menu
+drop constraint unique_member_dt;
+
+-- 사용하지 않는 컬럼 삭제
+alter table lunch_menu 
+drop column member_name;
+
+-- member_id null 값을 허용하지 않도록 설정
+-- delete from lunch_menu;
+alter table lunch_menu
+alter column member_id set not null;
+
+-- 새로운 제약조건 추가
+alter table lunch_menu
+add constraint unique_memberid_dt unique (member_id, dt);
+
+-- 맵버 테이블에 기본키를 설정
+alter table member
+add constraint member_id_pk primary key(id);
+
+-- 관계 조건 추가
+alter table lunch_menu
+add constraint menu_member_fk 
+	foreign key (member_id)
+	REFERENCES  member(id)
+;
+-- SQL Error [42830]: ERROR: there is no unique constraint matching given keys for referenced table "member"
+
+-- 테스트
+select * from member;
+select max(id) from member; -- 10
+
+insert into lunch_menu(menu_name, member_id, dt)
+values('순대국', 1, '2025-01-01');
+
+select * from lunch_menu;
+
+insert into lunch_menu(menu_name, member_id, dt)
+values('순대국', 11, '2025-01-01');
+-- SQL Error [23503]: ERROR: insert or update on table "lunch_menu" violates foreign key constraint "menu_member_fk"
 ```
 
 SELECT jsonb_object_agg(name, id) 
