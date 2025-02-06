6d13cf7bcdefc1a4e3731593cb30f22708e2b56c
ppabam <tom@diginori.com>
2025-02-06 09:43:23 +0900

    먹은 사람 선택으로 전환

--- App.py
+++ App.py
@@ -40,9 +40,8 @@ st.title(f"순신점심기록장!{db_name}")
 
 st.subheader("입력")
 menu_name = st.text_input("메뉴 이름", placeholder="예: 김치찌게")
-member_name = st.text_input("먹은 사람", value="TOM")
-option = st.selectbox(
-    "먹은 사람 선택",
+member_name = st.selectbox(
+    "먹은 사람",
     ("TOM", "KIM", "LEE"),
 )
 dt = st.date_input("얌얌 날짜")
