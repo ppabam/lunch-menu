244abcd21598603de6abef76aba21f0a0ad2d788
ppabam <tom@diginori.com>
2025-02-06 17:11:36 +0900

    JOIN 을 사용해 SELECT - 이름 표시

--- App.py
+++ App.py
@@ -66,12 +66,16 @@ if isPress:
 
 
 st.subheader("확인")
-query = """SELECT 
-menu_name AS menu, 
-member_name AS ename, 
-dt 
-FROM lunch_menu 
-ORDER BY dt DESC"""
+query = """
+SELECT
+	l.menu_name,
+	m.name,
+	l.dt
+FROM
+	lunch_menu l
+	inner join member m
+	on l.member_id = m.id
+"""
 
 conn = get_connection()
 cursor = conn.cursor()
