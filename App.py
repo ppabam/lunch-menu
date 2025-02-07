a6877df19bb98d1fbf0bdc5e4f9b0071184151ff
ppabam <tom@diginori.com>
2025-02-07 10:30:43 +0900

    mv get_conn

--- App.py
+++ App.py
@@ -1,25 +1,11 @@
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
-import psycopg
-import os
-from dotenv import load_dotenv
+from lunch_menu.db import get_connection, db_name
+
 
 members = {"SEO": 5, "TOM": 1, "cho": 2, "hyun": 3, "nuni": 10, "JERRY": 4, "jacob": 7, "jiwon": 6, "lucas": 9, "heejin": 8}
 
-# https://docs.streamlit.io/develop/concepts/connections/secrets-management
-load_dotenv()
-db_name = os.getenv("DB_NAME")
-DB_CONFIG = {
-    "user": os.getenv("DB_USERNAME"),
-    "dbname": db_name,
-    "password": os.getenv("DB_PASSWORD"),
-    "host": os.getenv("DB_HOST"),
-    "port": os.getenv("DB_PORT") 
-}
-
-def get_connection():
-    return psycopg.connect(**DB_CONFIG)
 
 def insert_menu(menu_name, member_id, dt):
     try:
