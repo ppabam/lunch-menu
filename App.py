d63932fa4e1441661a81752e65dd4c6f639161a0
ppabam <tom@diginori.com>
2025-02-07 11:06:45 +0900

    mv select_table

--- App.py
+++ App.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
-from lunch_menu.db import get_connection, db_name, insert_menu
+from lunch_menu.db import get_connection, db_name, insert_menu, select_table
 
 
 members = {"SEO": 5, "TOM": 1, "cho": 2, "hyun": 3, "nuni": 10, "JERRY": 4, "jacob": 7, "jiwon": 6, "lucas": 9, "heejin": 8}
@@ -33,37 +33,14 @@ if isPress:
 
 
 st.subheader("확인")
-query = """
-SELECT
-	l.menu_name,
-	m.name,
-	l.dt
-FROM
-	lunch_menu l
-	inner join member m
-	on l.member_id = m.id
-"""
-
-conn = get_connection()
-cursor = conn.cursor()
-cursor.execute(query)
-rows = cursor.fetchall()
-cursor.close()
-conn.close()
-
-#selected_df = pd.DataFrame([[1,2,3],[4,5,6]], columns=['a','b','c'])
-select_df = pd.DataFrame(rows, columns=['menu','ename','dt'])
+select_df = select_table()
 select_df
 
 st.subheader("통계")
-
-#gdf = not_na_df.groupby('ename')['menu'].count().reset_index()
 gdf = select_df.groupby('ename')['menu'].count().reset_index()
-#gdf.plot(x="ename", y="menu", kind="bar")
-
 gdf
 
-# 📊 Matplotlib로 바 차트 그리기
+st.subheader("차트")
 # https://docs.streamlit.io/develop/api-reference/charts/st.pyplot
 try:
     fig, ax = plt.subplots()
@@ -73,8 +50,6 @@ except Exception as e:
     st.warning(f"차트를 그리기에 충분한 데이터가 없습니다")
     print(f"Exception:{e}")
 
-# TODO
-# CSV 로드해서 한번에 다 디비에 INSERT 하는거
 st.subheader("벌크 인서트")
 if st.button("한방에 인서트"):
     df = pd.read_csv('note/menu.csv')
--- src/lunch_menu/db.py
+++ src/lunch_menu/db.py
@@ -1,6 +1,7 @@
 import psycopg
 from dotenv import load_dotenv
 import os
+import pandas as pd
 
 # https://docs.streamlit.io/develop/concepts/connections/secrets-management
 load_dotenv()
@@ -34,4 +35,26 @@ def insert_menu(menu_name, member_id, dt):
     except Exception as e:
         print(f"Exception:{e}")
         return False
+
+def select_table():
+    query = """
+    SELECT
+        l.menu_name,
+        m.name,
+        l.dt
+    FROM
+        lunch_menu l
+        inner join member m
+        on l.member_id = m.id
+    """
+
+    conn = get_connection()
+    cursor = conn.cursor()
+    cursor.execute(query)
+    rows = cursor.fetchall()
+    cursor.close()
+    conn.close()
+
+    df = pd.DataFrame(rows, columns=['menu','ename','dt'])
+    return df
 
